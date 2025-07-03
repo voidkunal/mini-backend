0bed46d26a609ba1b96a1dbb5fee4f0abac1b52d
voidkunal <kunalmandal1607@gmail.com>
2025-07-03 13:08:45 +0530

    authRouter

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -13,13 +13,16 @@ import { isAuthenticated } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
+// Public Routes
 router.post("/register", register);
 router.post("/verify-otp", verifyOTP);
 router.post("/login", login);
-router.get("/logout", isAuthenticated, logout);
-router.get("/me", isAuthenticated, getUser);
 router.post("/password/forgot", forgotPassword);
 router.put("/password/reset/:token", resetPassword);
+
+// Protected Routes
+router.get("/logout", isAuthenticated, logout);
+router.get("/me", isAuthenticated, getUser);
 router.put("/password/update", isAuthenticated, updatePassword);
 
 export default router;
